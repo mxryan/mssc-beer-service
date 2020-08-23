f31b85adcf7378bff321b5e6e9e4012bd791cd10
Michael Ryan <mxryan@live.com>
2020-08-22 21:32:23 -0400

    bean validation

--- src/main/java/mxryan/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/mxryan/msscbeerservice/web/model/BeerDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -14,13 +18,25 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerDto {
+    // These props are @Null because these properties are set by the server and the client has no
+    // need to send them up. If a client tried to change one of these fields they now can't.
+    @Null
     private UUID id;
+    @Null
     private Integer version;
+    @Null
     private OffsetDateTime createdDate;
+    @Null
     private OffsetDateTime lastModifiedDate;
+    @NotBlank
     private String beerName;
+    @NotNull
     private BeerStyle beerStyle;
+    @NotNull
+    @Positive
     private Long upc;
+    @NotNull
+    @Positive
     private BigDecimal price;
     private Integer quantityOnHand;
 }
